3ce52bc372135a87de8c6197e1392b6dae4770d4
Remimstr <remievan@hotmail.com>
2018-11-23 16:40:10 -0500

    Add fish greeting, sudo last command, and custom vim bindings for fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
   for mode in insert default visual
     bind -M $mode \cf forward-char
+    bind -M insert ii "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
   end
 end
