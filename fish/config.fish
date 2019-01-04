6d9f403daa80f1a65836a711839f113b357e3ab4
Remimstr <remievan@hotmail.com>
2019-01-04 11:54:24 -0500

    Add ruby to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,2 @@
 thefuck --alias | source
-set fish_user_paths /home/remi/.nvm/versions/node/v8.12.0/bin /usr/local/bin
+set fish_user_paths /home/remi/.nvm/versions/node/v11.5.0/bin /usr/local/bin /home/remi/.gem/ruby/2.5.0/bin
---
+++ fish/functions/kotlinr.fish
@@ -0,0 +1,5 @@
+function kotlinr
+  echo "Compiling, please wait..."
+  kotlinc $argv -include-runtime -d out.jar
+  java -jar out.jar
+end
